8259bbb61cc51ad44a8cd1aa99dce3a9453a6472
James Fraser-Jones <jf14218@my.bristol.ac.uk>
2019-10-07 21:13:43 +0100

    yep yep yep

--- dev/src/otter.elm
+++ dev/src/otter.elm
@@ -7,8 +7,8 @@ module Otter exposing (..)
     elm reactor
     elm install NAME/PACKAGE
     elm make --output=FILENAME.js FILENAME.elm
-    ../../nwjs-sdk-v0.40.2-win-x64/otter.exe .
-    ../../nwjs-sdk-v0.40.2-win-x64/nwjc.exe src/otter.js bin/otter.bin
+    ../nwjs/nwjs-sdk-v0.40.2-win-x64/nw.exe .
+    ../nwjs/nwjs-sdk-v0.40.2-win-x64/nwjc.exe src/otter.js bin/otter.bin
 -}
 --==================================================================== PACKAGE DEPENDENCIES
 {-
