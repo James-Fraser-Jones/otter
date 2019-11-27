c91e6917d17bac13c2179563dfe7b30df65b8fa6
James Fraser-Jones <jf14218@my.bristol.ac.uk>
2019-11-27 14:54:39 +0000

    pemplate, only settings to do now

--- misc/template3.js
+++ misc/template3.js
@@ -1,1 +1,29 @@
-$('.ui.dropdown').dropdown();
+$('.dropdown').dropdown({transition: 'slide down', on: 'hover' }); //auto dropdown on hover menu items
+$('.ui.modal').modal(); //set up for settings modal
+
+function send_error(msg){
+  $('body').toast({
+    class: 'red',
+    position: 'bottom center',
+    title: 'Error:',
+    showIcon: 'microphone',
+    closeIcon: true,
+    displayTime: 0,
+    message: msg
+  });
+}
+
+function send_info(msg){
+  $('body').toast({
+    class: 'blue',
+    position: 'bottom center',
+    showIcon: 'microphone',
+    closeIcon: true,
+    displayTime: 0,
+    message: msg
+  });
+}
+
+function open_settings(){
+  $('.ui.modal').modal('show');
+}
