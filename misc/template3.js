84f4af26b71587efae50c940c45be66e5dfb6f15
James Fraser-Jones <jf14218@my.bristol.ac.uk>
2019-11-29 14:06:09 +0000

    finished settings modal

--- misc/template3.js
+++ misc/template3.js
@@ -1,10 +1,8 @@
 $('.dropdown').dropdown({transition: 'slide down', on: 'hover' }); //auto dropdown on hover menu items
-$('.ui.modal').modal(); //set up for settings modal
 
 function send_error(msg){
   $('body').toast({
     class: 'red',
-    title: 'Error:',
     position: 'bottom center',
     showIcon: 'exclamation',
     displayTime: 0,
@@ -15,7 +13,6 @@ function send_error(msg){
 function send_info(msg){
   $('body').toast({
     class: 'blue',
-    title: 'Info:',
     position: 'bottom center',
     showIcon: 'info',
     displayTime: 0,
@@ -25,8 +22,4 @@ function send_info(msg){
 
 function close_messages(){
   $('.ui.toast').toast('close');
-}
-
-function open_settings(){
-  $('.ui.modal').modal('show');
 }
