05fa4fa7d40370311c96af5f9c1a14b4cbcd793c
James Fraser-Jones <jf14218@my.bristol.ac.uk>
2019-11-30 15:47:22 +0000

    preliminary merging of template files to dev

--- dev/src/Ports.elm
+++ dev/src/Ports.elm
@@ -1,3 +1,8 @@
 port module Ports exposing (..)
 
 port example : String -> Cmd msg
+port send_error : String -> Cmd msg
+port send_info : String -> Cmd msg
+port close_all : () -> Cmd msg
+port close_newest : () -> Cmd msg
+port close_oldest : () -> Cmd msg
--- dev/src/otter_custom.js
+++ dev/src/otter_custom.js
@@ -1,6 +1,61 @@
+function send_error(msg){
+  $('body').toast({
+    class: 'red',
+    position: 'bottom center',
+    showIcon: 'exclamation',
+    displayTime: 0,
+    message: msg
+  });
+}
+
+function send_info(msg){
+  $('body').toast({
+    class: 'blue',
+    position: 'bottom center',
+    showIcon: 'info',
+    displayTime: 0,
+    message: msg
+  });
+}
+
+function close_all(){
+  $('.ui.toast').toast('close');
+}
+
+function close_newest(){
+  $('.ui.toast').last().toast('close');
+}
+
+function close_oldest(){
+  $('.ui.toast').first().toast('close');
+}
+
 var app = Elm.Otter.init({
   node: document.getElementById('elm')
 });
+
+$('.dropdown').dropdown({transition: 'slide down', on: 'hover' }); //auto dropdown on hover menu items
+
 app.ports.example.subscribe(function(data) {
   console.log("Example: " + data);
+});
+
+app.ports.send_error.subscribe(function(msg) {
+  send_error(msg);
+});
+
+app.ports.send_info.subscribe(function(msg) {
+  send_info(msg);
+});
+
+app.ports.close_all.subscribe(function() {
+  close_all();
+});
+
+app.ports.close_newest.subscribe(function() {
+  close_newest();
+});
+
+app.ports.close_oldest.subscribe(function() {
+  close_oldest();
 });
