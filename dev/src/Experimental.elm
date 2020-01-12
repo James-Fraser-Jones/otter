3b91623800cd8d690f4c7e62915b7773f8365667
James Fraser-Jones <jf14218@my.bristol.ac.uk>
2020-01-12 17:10:14 +0000

    Added type application

--- dev/src/Experimental.elm
+++ dev/src/Experimental.elm
@@ -1,9 +1,13 @@
 module Experimental exposing (..)
 
+--------------------------------------------------------------------------------
+--Custom Types
+
 type Type
   = Prim Primitive
   | Cont Container
   | New NewType
+  | Var TVar
 --deriving (Show, Read, Encoder, Decoder, Html)
 
 type Primitive
@@ -19,58 +23,128 @@ type Container
   | Set Type
   | Dict Type Type
   | Tuple Type Type
-  | Record Record
-
-type Record
-  = Rec (List (String, Type))
+  | Rec Record
 
 type NewType
   = Alias String Record
-  | Custom String (List (String, (List Type)))
+  | Cus Custom
 
-never : NewType
+--Aliased Records
+
+type alias Custom =
+  { name : String
+  , vars : List TVar
+  , cons : List (String, (List Type))
+  }
+
+type alias Record =
+  { rec : List (String, Type)
+  }
+
+type alias TVar =
+  { tvar : String
+  }
+
+type alias Elm =
+  { elm : String
+  }
+
+--------------------------------------------------------------------------------
+--Common custom types
+
+never : Custom
 never =
-  Custom "Never" []
+  Custom "Never" [] []
 
-unit : NewType
+unit : Custom
 unit =
-  Custom "()" [("()",[])]
+  Custom "()" [] [("()",[])]
 
-bool : NewType
+bool : Custom
 bool =
-  Custom "Bool" [("True",[]), ("False",[])]
+  Custom "Bool" [] [("True",[]), ("False",[])]
 
-maybe : Type -> NewType
-maybe a =
-  Custom "Maybe" [("Nothing", []), ("Just", [a])]
+maybe : Custom
+maybe =
+  Custom "Maybe" [TVar "a"] [("Nothing", []), ("Just", [Var <| TVar "a"])]
 
-result : Type -> NewType
-result error value =
-  Custom "Result" [("Ok", [value]), ("Err", [error])]
+result : Custom
+result =
+  Custom "Result" [TVar "error", TVar "value"] [("Ok", [Var <| TVar "value"]), ("Err", [Var <| TVar "error"])]
 
-order : NewType
+order : Custom
 order =
-  Custom "Order" [("LT",[]), ("EQ",[]), ("GT",[])]
+  Custom "Order" [] [("LT",[]), ("EQ",[]), ("GT",[])]
 
-endianness : NewType
+endianness : Custom
 endianness =
-  Custom "Endianness" [("LE",[]), ("BE",[])]
+  Custom "Endianness" [] [("LE",[]), ("BE",[])]
 
-type Elm
-  = Elm String
+--------------------------------------------------------------------------------
+--type application of custom types
+
+typeApp : Type -> Custom -> Custom
+typeApp t c =
+  case c.vars of
+    [] -> c
+    (v :: vs) -> subCustom v t {c | vars = vs}
+
+subType : TVar -> Type -> Type -> Type
+subType x y t =
+  case t of
+    Prim p -> Prim (subPrimitive x y p)
+    Cont c -> Cont (subContainer x y c)
+    New n -> New (subNewType x y n)
+    Var v -> subTVar x y v
+
+subPrimitive : TVar -> Type -> Primitive -> Primitive
+subPrimitive _ _ p = p
+
+subContainer : TVar -> Type -> Container -> Container
+subContainer x y c =
+  case c of
+    List t -> List (subType x y t)
+    Array t -> Array (subType x y t)
+    Set t -> Set (subType x y t)
+    Dict t t2 -> Dict (subType x y t) (subType x y t2)
+    Tuple t t2 -> Tuple (subType x y t) (subType x y t2)
+    Rec r -> Rec (subRecord x y r)
+
+subNewType : TVar -> Type -> NewType -> NewType
+subNewType x y n =
+  case n of
+    Alias s r -> Alias s (subRecord x y r)
+    Cus c -> Cus (subCustom x y c)
+
+subCustom : TVar -> Type -> Custom -> Custom
+subCustom x y c =
+  let newCons = List.map (Tuple.mapSecond <| List.map <| subType x y) c.cons
+   in {c | cons = newCons}
+
+subRecord : TVar -> Type -> Record -> Record
+subRecord x y r =
+  let newRec = List.map (Tuple.mapSecond <| subType x y) r.rec
+   in {r | rec = newRec}
+
+subTVar : TVar -> Type -> TVar -> Type
+subTVar x y v =
+  if x == v then y else Var v
+
+--------------------------------------------------------------------------------
+--Outer level stuff
 
 --parseDecs : Elm -> Result String (List NewType)
 
---genPrinter : NewType -> Elm
---genParser : NewType -> Elm
---genEncoder : NewType -> Elm
---genDecoder : NewType -> Elm
---genHtml : NewType -> Elm --renders `Model` type using Generic deriving of it and all its children types
+--genPrinter : Type -> Elm
+--genParser : Type -> Elm
+--genEncoder : Type -> Elm
+--genDecoder : Type -> Elm
+--genHtml : Type -> Elm --renders `Model` type using Generic deriving of it and all its children types
 
 --parseMyType : String -> Result String MyType
 --printMyType : MyType -> String
 --decodeMyType : Decoder MyType
 --encodeMyType : MyType -> Encode.Value
---htmlMyType : MyType -> Html GenMsg 
+--htmlMyType : MyType -> Html GenMsg
 
 --generate : String -> Cmd msg --actually call this code to scan some elm file on the file system for type declarations
